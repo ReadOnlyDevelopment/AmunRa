b7d3a53bc47699539bf251ba332715118d6c258c
ROMVoid95 <rom.void95@gmail.com>
2020-06-05 12:47:44 -0500

    add 2 unsaved classes

--- src/main/java/de/katzenpapst/amunra/client/renderer/BlockRendererDummy.java
+++ src/main/java/de/katzenpapst/amunra/client/renderer/BlockRendererDummy.java
@@ -2,42 +2,38 @@ package de.katzenpapst.amunra.client.renderer;
 
 import de.katzenpapst.amunra.AmunRa;
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 /**
  * A renderer to simply don't render blocks, for when the tile entity is supposed to do the rendering
- *
  */
 public class BlockRendererDummy implements ISimpleBlockRenderingHandler {
 
-    public BlockRendererDummy() {
-        AmunRa.dummyRendererId = RenderingRegistry.getNextAvailableRenderId();
-    }
+	public BlockRendererDummy() {
+		AmunRa.dummyRendererId = RenderingRegistry.getNextAvailableRenderId();
+	}
 
-    @Override
-    public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
-        // don't
-    }
+	@Override
+	public int getRenderId() {
+		return AmunRa.dummyRendererId;
+	}
 
-    @Override
-    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
-            RenderBlocks renderer) {
-        // don't do anything
-        return true;
-    }
+	@Override
+	public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
+		// don't
+	}
 
-    @Override
-    public boolean shouldRender3DInInventory(int modelId) {
-        // ?
-        return true;
-    }
+	@Override
+	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+		// don't do anything
+		return true;
+	}
 
-    @Override
-    public int getRenderId() {
-        return AmunRa.dummyRendererId;
-    }
+	@Override
+	public boolean shouldRender3DInInventory(int modelId) {
+		// ?
+		return true;
+	}
 
 }
--- src/main/java/de/katzenpapst/amunra/old/tile/TileEntityARChestLarge.java
+++ src/main/java/de/katzenpapst/amunra/old/tile/TileEntityARChestLarge.java
@@ -4,21 +4,20 @@ import net.minecraft.item.ItemStack;
 
 public class TileEntityARChestLarge extends TileEntityARChest {
 
-    public TileEntityARChestLarge() {
-        chestContents = new ItemStack[this.getSizeInventory()];
-    }
+	public TileEntityARChestLarge() {
+		chestContents = new ItemStack[this.getSizeInventory()];
+	}
 
-    /**
-     * Returns the number of slots in the inventory.
-     */
-    @Override
-    public int getSizeInventory()
-    {
-        return 54;
-    }
+	@Override
+	protected boolean canDoublechest() {
+		return false;
+	}
 
-    @Override
-    protected boolean canDoublechest() {
-        return false;
-    }
+	/**
+	 * Returns the number of slots in the inventory. mj
+	 */
+	@Override
+	public int getSizeInventory() {
+		return 54;
+	}
 }
